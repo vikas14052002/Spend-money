28d163a8c1bbb5bf4f4a4dbb11e1ef723cf936d1
Vikas <69838175+devilAxel-777@users.noreply.github.com>
2021-09-04 01:41:50 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,198 @@
+'use strict';
+// Elements
+let totalMoneyElement = document.querySelector('#totalMoney');
+let percentageElement = document.querySelector('#percentageLeft');
+let buyButtons = document.querySelectorAll('#buy');
+let sellButtons = document.querySelectorAll('#sell');
+const appContainer = document.querySelector('.app-container');
+
+// Default data
+let elonFortune = 164000000000;
+let totalPercentage = 100;
+
+// Events
+appContainer.addEventListener('click', (e) => {
+  let element = e.target.parentElement;
+
+  if (e.target.id === 'buy') {
+    buyItem(element);
+  } else if (e.target.id === 'sell') {
+    sellItem(element);
+  }
+});
+
+// Buy item
+function buyItem(element) {
+  // change default data to new data
+
+  if (elonFortune - Number(element.dataset.price) >= 0) {
+    elonFortune -= Number(element.dataset.price);
+    totalPercentage = (elonFortune * 100) / 164000000000;
+
+    // Item name
+    let itemName = element.parentElement.querySelector('p').textContent;
+
+    // get span to increment by one
+    let amountOfItems = element.querySelector('span');
+    amountOfItems.textContent = `${Number(amountOfItems.textContent) + 1}`;
+
+    // get button to enable it when item is more than 0
+    let button = element.querySelector('#sell');
+    if (Number(amountOfItems.textContent) > 0) {
+      button.disabled = false;
+    }
+
+    updateTotalAndPercentage();
+
+    // Create (if its new) or update recipt item(if it already exists)
+    createReciptItem(
+      itemName,
+      Number(amountOfItems.textContent),
+      formatMoney(
+        Number(element.dataset.price) * Number(amountOfItems.textContent)
+      )
+    );
+
+    updateReceipt();
+  } else {
+    cantAffordAlert();
+  }
+}
+
+function cantAffordAlert() {
+  totalMoneyElement.innerHTML = `<p class="totalMoney">Can't afford that!</p>`;
+  percentageElement.innerHTML = `<p class ="percentageLeft">Sell something!</p>`;
+}
+
+function createReciptItem(name, amount, total) {
+  let receiptItem = new ReceiptItem();
+  receiptItem.name = name;
+  receiptItem.amount = amount;
+  receiptItem.total = total;
+
+  if (!checkReceiptItemExists(receiptItem)) {
+    receiptItemsArr.push(receiptItem);
+  } else {
+    updateReceiptItem(receiptItem);
+  }
+}
+
+// Sell Item
+function sellItem(element) {
+  // change default data to new data
+
+  elonFortune += Number(element.dataset.price);
+  totalPercentage = (elonFortune * 100) / 164000000000;
+
+  // Item name
+  let itemName = element.parentElement.querySelector('p').textContent;
+
+  // get span to decrement by one
+  let amountOfItems = element.querySelector('span');
+  amountOfItems.textContent = `${Number(amountOfItems.textContent) - 1}`;
+
+  // get button to disable when item is less than 0
+  let button = element.querySelector('#sell');
+
+  if (Number(amountOfItems.textContent) === 0) {
+    button.disabled = true;
+  }
+  updateTotalAndPercentage();
+
+  createReciptItem(
+    itemName,
+    Number(amountOfItems.textContent),
+    formatMoney(
+      Number(element.dataset.price) * Number(amountOfItems.textContent)
+    )
+  );
+
+  updateReceipt();
+}
+
+function updateTotalAndPercentage() {
+  totalMoneyElement.innerHTML = `<p class="totalMoney">Remaining: ${formatMoney(
+    elonFortune
+  )} USD</p>`;
+  percentageElement.innerHTML = `<p class ="percentageLeft">You only spent ${(
+    100 - totalPercentage
+  ).toFixed(6)} % of the total!</p>`;
+}
+
+// Format Money Function
+function formatMoney(number) {
+  return number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+}
+
+// Class to create unique receipt items
+class ReceiptItem {
+  constructor() {
+    this.name;
+    this.amount;
+    this.total;
+  }
+}
+
+let receiptItemsArr = [];
+
+// Function that check if that receipt items its already added on the array
+function checkReceiptItemExists(receiptItem) {
+  let i = 0;
+  let exists = false;
+
+  while (!exists && i < receiptItemsArr.length) {
+    let itemX = receiptItemsArr[i];
+    if (itemX.name === receiptItem.name) {
+      exists = true;
+    }
+    i++;
+  }
+
+  return exists;
+}
+
+function updateReceiptItem(receiptItem) {
+  let i = 0;
+  let itemInArr = null;
+
+  while (itemInArr === null && i < receiptItemsArr.length) {
+    let itemX = receiptItemsArr[i];
+
+    if (itemX.name === receiptItem.name) {
+      itemInArr = itemX;
+    }
+    i++;
+  }
+
+  if (itemInArr) {
+    itemInArr.name = receiptItem.name;
+    itemInArr.amount = receiptItem.amount;
+    itemInArr.total = receiptItem.total;
+  }
+}
+
+// Function to create recipt (iterara por el array y mostrara los objetos en una lista)
+function updateReceipt() {
+  let title = `<h1>Receipt</h1>`;
+  let receipt = '';
+  let total = formatMoney(164000000000 - elonFortune);
+
+  for (let i = 0; i < receiptItemsArr.length; i++) {
+    let itemX = receiptItemsArr[i];
+
+    if (itemX.amount !== 0) {
+      receipt += `<p>${itemX.name} x <strong> ${itemX.amount}</strong> ......................$ ${itemX.total}</p>`;
+    }
+  }
+
+  document.querySelector('#receipt-container').innerHTML =
+    title + receipt + `<p class="totalRecipt">Total is: $ ${total}</p>`;
+}
+
+// Function to print
+function printSection(el) {
+  let printsection = document.getElementById(el).innerHTML;
+  document.body.innerHTML = printsection;
+
+  window.print();
+}
